6f4b44f89dc03f648ce77279bcd8d3c8755a0e97
Johny <joeljoshy500@gmail.com>
2021-05-21 13:05:09 +0530

    Example of stack

--- Advanced Python/Datastructure/Stack.py
+++ Advanced Python/Datastructure/Stack.py
@@ -5,9 +5,9 @@ stack = []
 def push():
 
     if len(stack) < size:
-        element1 = input("Enter the element to push:")
-        stack.append(element1)
-        print("Stack : ", stack)
+        element = input("Enter the element to push:")
+        stack.append(element)
+        print("Stack : ", stack[::-1])
     else:
         print("Stack is full")
 
@@ -18,7 +18,7 @@ def pop():
 
     else:
         stack.pop()
-        print("Stack : ", stack)
+        print("Stack : ", stack[::-1])
 
 
 while True:
